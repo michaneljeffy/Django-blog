a65f41898520fdad00793e3965f6f4e062d8e5de
michaneljeffy <845072805@qq.com>
2017-11-21 22:52:22 +0800

    'addadmintool'l'

--- TestModel/admin.py
+++ TestModel/admin.py
@@ -3,4 +3,23 @@ from django.contrib import admin
 from TestModel.models import Test,Contact,Tag
 # Register your models here.
 
-admin.site.register([Test,Contact,Tag])
+class TagInline(admin.TabularInline):
+    """Tag内联"""
+    model = Tag
+
+class ContactAdmin(admin.ModelAdmin):
+    """新建类"""
+    list_display = ('name','age', 'email') # list
+    search_fields = ('name',)
+    inlines = [TagInline]  # Inline
+    fieldsets = (
+        ['Main', {
+            'fields': ('name', 'email'),
+        }],
+        ['Advance', {
+            'classes': ('collapse',), # CSS
+            'fields': ('age',),
+        }]
+    )
+admin.site.register(Contact,ContactAdmin);
+admin.site.register(Test)
